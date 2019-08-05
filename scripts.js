eb45b86b774902b9dceefe8ba3f1f94ac71974b6
BrendaMwiza <brendabrizy@gmail.com>
2019-08-05 13:45:18 +0200

    eddit

--- scripts.js
+++ scripts.js
@@ -4,16 +4,13 @@ function order(size, crust, toping, number, price){
     this.topings = toping;
     this.desiredNumber = number;
     this.correspondingPrice = price;
-    this.addItAll = function() {
-        return this.correspondingPrice;
-    }
+    
 };
 
-var smallSize = new order("small", "crust", "toping", "number", "$12");
-
-var mediumSize = new order("medium", "crust", "toping", "number", "$14");
-var largeSize = new order("large", "crust", "toping", "number", "$16");
-var extraLargeSize = new order("extralarge", "crust", "toping", "number", "$30");
+var smallSize = new order("small", "crust", "toping", "number", "12");
+var mediumSize = new order("medium", "crust", "toping", "number", "14");
+var largeSize = new order("large", "crust", "toping", "number", "16");
+var extraLargeSize = new order("extralarge", "crust", "toping", "number", "30");
 console.log(smallSize);
 console.log(mediumSize);
 console.log(largeSize);
@@ -21,13 +18,30 @@ console.log(extraLargeSize);
 
 
 
+
 document.getElementById("submit").addEventListener("click", function() {
     var location = prompt("pease enter your location:", " ");
-    var write;
-    if (location == null || location == "") {
-        write = "you cancelled order.";
-      } else {
-        alert("Hello " + "the amout you have to pay is" + this.addItAll );
-      };
-    
+    if(size==small){
+        alert("Hello " + "the amout you have to pay is" + "$" + smallSize.correspondingPrice );
+
+    }
+    else if(size==medium){
+        alert("Hello " + "the amout you have to pay is" + "$" + mediumSize.correspondingPrice );
+
+    }
+    else if(size==large){
+        alert("Hello " + "the amout you have to pay is" + "$" + largeSize.correspondingPrice );
+
+    }
+    else if(size==extraLarge){
+        alert("Hello " + "the amout you have to pay is" + "$" + extraLargeSize.correspondingPrice );
+
+    }
+    else{
+        alert("You canceled the order");
+    };
+        
+
 });
+    
+    
