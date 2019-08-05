2ad186c56f52df41509fb509ceaacf55a387731c
BrendaMwiza <brendabrizy@gmail.com>
2019-08-05 10:24:28 +0200

    add function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
-function order(size, crust, toping, number){
+function order(size, crust, toping, number, price){
     this.sizes = size;
     this.crustName = crust;
     this.topings = toping;
     this.desiredNumber = number;
-    
-
-
+    this.correspondingPrice = price;
 }
+
+var smallSize = new order("small", "crust", "toping", "number", $12);
+var mediumSize = new order("medium", "crust", "toping", "number", $14);
+var largeSize = new order("large", "crust", "toping", "number", $16);
+var extraLargeSize = new order("extralarge", "crust", "toping", "number", $30);
